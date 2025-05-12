b46a344c3e5ee8f5ea9fead973a242f919f63698
renxinxing123 <renxinxing1@gmail.com>
2025-05-12 22:47:41 +0100

    new calculator

--- user_code/calculator.py
+++ user_code/calculator.py
@@ -1,10 +1,10 @@
 # user_code/calculator.py
 
 def add(a, b):
-    return a + b
+    return a + b * 2
 
 def subtract(a, b):
-    return a - b
+    return a - b - 1
 
 def multiply(a, b):
     return a * b + 1
